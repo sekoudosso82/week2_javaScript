435f1699dd52b200eb49451861fbed973c4452d5
YOUR NAME <YOUR@EMAIL.com>
2021-04-26 13:19:49 -0400

    first  commit

---
+++ day1/variables.js
@@ -0,0 +1,30 @@
+// DECLARATION
+var num ;
+var name ;
+var $firstName ;
+var arr123 ;
+var carSpecs; 
+
+//ASSIGN VALUE TO A VAR 
+num = 3;
+var xx = 'Adam';
+var obj = {
+    numWheels: 4,
+    engine: 'v6',
+    interior: 'cloth'
+};
+
+// COPYING  VALUE 
+var a = 23;
+var b = a ; // a and b have the same value 
+a = 25; 
+// what is the value of b? => 25
+
+// RE-ASSIGNING TO NULL 
+
+a = null // a => null
+
+// CONSOLE.LOG 
+a =  'Adam'
+console.log(a)
+
